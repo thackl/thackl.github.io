93aab9506d648083ca00729b47b40d5ec25c73d4
Thomas Hackl <thomas.hackl@uni-wuerzburg.de>
2018-09-23 05:26:47 -0400

    improved publication list

--- _publication/google-scholar.R
+++ _publication/google-scholar.R
@@ -23,18 +23,20 @@ html_2 <- html_1 %>%
 #    author=str_replace_all(author, " (\\S) ", "\\1 "),
     author=str_replace_all(author, "([A-Z]) ([A-Z]) ", "\\1\\2 "),
     author=str_replace_all(author, ", \\.\\.\\.", " et al."),
-    author=str_replace_all(author, "T Hackl", "<b>T Hackl</b>"),
+    author=str_replace_all(author, "T Hackl", "<b>T Hackl</b>")
   ) %>% split(.$year) %>%
     map(function(x){
-      x %>% glue_data('<p>{author} ({year}) <a href="https://scholar.google.com/scholar?oi=bibs&cluster={cid}&btnI=1&hl=en">{title}</a>, {journal}, {number}</p>') %>% str_replace_all("(, )+</p>", "</p>") %>%
-  char2html()
+      x <- x %>%
+        glue_data('<tr><td width="100%">{author} ({year}) <a href="https://scholar.google.com/scholar?oi=bibs&cluster={cid}&btnI=1&hl=en">{title}</a>, {journal}, {number}</td></tr>') %>%
+        str_replace_all("(, )+</p>", "</p>") %>%
+        char2html()
+      x <- c('<table class="publication-table" border="10px solid blue" cellspacing="0" cellpadding="6" rules="", frame=""><tbody>', x, '</tbody></table>')
+      return(x);
     }) %>% rev
 
 html_3 <- map2(names(html_2) %>% paste0("<h3>", ., "</h3>"), html_2, c) %>% unlist
 
 html_4 <- c(
-  paste0('<p style="text-align: right; margin-top: -40px;"><small>Last updated <i>', format(Sys.Date(), format="%B %d, %Y"), '</i>. Pulled from my <a href="https://scholar.google.com/citations?hl=en&user=b8bWNkUAAAAJ">Google Scholar profile</a></small></p>'),
-  '<div class="publication-list">',
-  html_3, '</div>')
+  paste0('<p style="text-align: right; margin-top: -40px;"><small>Last updated <i>', format(Sys.Date(), format="%B %d, %Y"), '</i>. Pulled from my <a href="https://scholar.google.com/citations?hl=en&user=b8bWNkUAAAAJ">Google Scholar profile</a></small></p>'), html_3)
 writeLines(html_4, "../_includes/publications.html")
 
